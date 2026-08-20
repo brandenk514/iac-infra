a4634e97b4b2f2784c96bcc54e1f7729b3233774
brandenk514 <12478583+brandenk514@users.noreply.github.com.>
2026-08-20 09:30:21 -0600

    Updated valkey image hash

--- opentofu/pterodactyl/immich.tf
+++ opentofu/pterodactyl/immich.tf
@@ -109,7 +109,7 @@ resource "docker_container" "immich_ml" {
 # Valkey – Cache for Immich (Redis-compatible)
 # ---------------------------------------------------------------------------
 resource "docker_image" "redis" {
-  name = "docker.io/valkey/valkey:9@sha256:8e8d64b405ce18f41b8e5ee20aa4687a8ed0022d1298f2ce31cdcf3a76e09411"
+  name = "docker.io/valkey/valkey:9@sha256:3acc0687f2a2e1091fae6450d7842dd658c941338cf0a873ddd9e14b9e4ea4dd"
 }
 
 resource "docker_container" "immich_redis" {
