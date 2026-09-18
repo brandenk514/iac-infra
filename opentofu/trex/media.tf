147d8252f06926244dc1ddabb35d96b0c73bb0ce
brandenk514 <12478583+brandenk514@users.noreply.github.com.>
2026-09-18 16:54:49 -0600

    reverted filters

--- opentofu/trex/media.tf
+++ opentofu/trex/media.tf
@@ -246,7 +246,7 @@ resource "docker_container" "tovpn" {
     "OPENVPN_CONFIG=",
     "OPENVPN_USERNAME=${var.openvpn_username}",
     "OPENVPN_PASSWORD=${var.openvpn_password}",
-    "LOCAL_NETWORK=192.168.100.0/24, 192.168.105.0/24, ${docker_network.proxy.subnet}",
+    "LOCAL_NETWORK=192.168.100.0/24, 192.168.105.0/24",
     "TRANSMISSION_WEB_HOME=/opt/transmission-ui/flood-for-transmission",
   ]
 
