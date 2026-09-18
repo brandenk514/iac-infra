76cb14ae66eee2de0545a0a69f55ffafbac92c23
brandenk514 <12478583+brandenk514@users.noreply.github.com.>
2026-09-18 16:47:07 -0600

    Updated transmission whitelist var

--- opentofu/trex/media.tf
+++ opentofu/trex/media.tf
@@ -248,6 +248,7 @@ resource "docker_container" "tovpn" {
     "OPENVPN_PASSWORD=${var.openvpn_password}",
     "LOCAL_NETWORK=192.168.100.0/24, 192.168.105.0/24",
     "TRANSMISSION_WEB_HOME=/opt/transmission-ui/flood-for-transmission",
+    "TRANSMISSION_WHITELIST_ENABLED=false",
   ]
 
   log_driver = "json-file"
